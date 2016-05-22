633e1709495dc5fcb16958c75905a3a629ea560d
kenokabe <kenokabe@gmail.com>
2016-05-22 21:50:56 +0900

    commit

--- index.jsx
+++ index.jsx
@@ -5,24 +5,51 @@
     const ClockElement = __Element(__
       .intervalSeq(Immutable.Range(), 100)
       .__(() => (<div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>)));
-    const __items = __(true);
-    const ListElement = __Element(__([__items])
-      .__(() => ((__items).map((item) => (<li>{item}</li>)))));
+    const __focusedList = __()
+      .__((val) => (__.log.t = val));
+    const __lists = __(true)
+      .__((__list) => {
+        __focusedList.t = __lists.length;
+        return __list;
+      });
+    const ListofListElement = __Element(__([__lists])
+      .__(() => ((__lists)
+        .map((list, i) => (<button
+          onClick = {() => (__.log.t = __focusedList.t = i)}>
+          {'List#' + (i + 1)}</button>)))));
     const InputElement = () => {
       const __value = __();
       const onChange = (e) => (__value.t = e.target.value);
-      const onClick = () => (__.log.t = __items.t = __value.t);
+      const onClick = () => {
+        __.log.t = __lists[__focusedList.t].t = __value.t;
+      };
+      const onClickNL = () => {
+        __lists.t = __(true); //new items-list
+        __.log.t = 'newList';
+      };
       const __seqEl = __([__value])
         .__((value) => (<div>
-        <input type="text" value={value} onChange={onChange}/>
-        <button onClick = {onClick}>{'NewToDo#' + (__items.length + 1)}</button></div>));
+          <h4>{'List#' + (__focusedList.t + 1)}
+          <button onClick = {onClickNL}>{'NewList'}</button></h4>
+            {__lists[__focusedList.t]
+            .map((item) => (<li>{item}</li>))}
+            <input type="text" value={value} onChange={onChange}/>
+            <button onClick = {onClick}>{'NewToDo#' + (__lists[__focusedList.t].length + 1)}</button>
+            </div>));
       __.log.__(() => (__value.t = ""));
-      __.log.t = "started!";
       return __Element(__seqEl);
     };
+    __lists.t = __(true); //new items-list
+    (() => {
+      const __delay = __
+        .intervalSeq(Immutable
+          .Seq.of("started!"), 1000)
+        .log()
+        .__(() => (__.log.t = "showInput"));
+    })();
     return (<div><h2>ToDo</h2>
       {ClockElement}<p/>
-      {ListElement}
+      {ListofListElement}<p/>
       {InputElement()}</div>);
   };
   const mount = ReactDOM.render(TodoElement(), document.getElementById('todo'));
